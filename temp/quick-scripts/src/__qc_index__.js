730b295d959a2b4192760e59059d086af912667b
Allen <243249165@qq.com>
2020-06-11 09:42:55 +0800

    删除无用文件

--- temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,8 +0,0 @@
-
-require('./assets/script/GlobalSet');
-require('./assets/script/base/BaseComponent');
-require('./assets/script/model/public/AudioManager');
-require('./assets/script/model/public/NodePoolManager');
-require('./assets/script/model/public/ResManger');
-require('./assets/script/structure/public/ResInfo');
-require('./assets/script/utils/LogUtil');
